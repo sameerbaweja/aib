c30372e2ebfc8f99e0d3d3349816cc09e12af25c
sameerbaweja <sameer.baweja@hotmail.com>
2016-09-28 14:23:14 +0530

    deleted all

--- src/main/java/com/afteribuy/repositories/ProductTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.afteribuy.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.afteribuy.entities.ProductType;
-
-public interface ProductTypeRepository extends JpaRepository<ProductType, Integer> {
-
-}
