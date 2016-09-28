b166998e59c884f17ee0172c6183939b4bc8ee47
sameerbaweja <sameer.baweja@hotmail.com>
2016-09-28 17:33:51 +0530

    Updated for datasource

--- src/main/java/com/afteribuy/config/ApplicationConfiguration.java
+++ src/main/java/com/afteribuy/config/ApplicationConfiguration.java
@@ -6,6 +6,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
 import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
+//import org.apache.tomcat.dbcp.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
